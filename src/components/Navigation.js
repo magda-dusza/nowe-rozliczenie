c0922ef13072aa288a0db0f43d3dfaf1370ea838
Magda <magdalena.dusza13@gmail.com>
2018-06-09 01:31:02 +0200

    style main menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,21 @@
 import {NavLink} from 'react-router-dom';
 import React from 'react';
+import './styles/Navigation.css';
 
 export class Navigation extends React.Component {
     render(){
         return(
-            <ul>
-                <li>Transakcje</li>
-                <li>Ustawienia</li>
-                <li>Statystyki</li>
-            </ul>
+            <div className="main-menu">
+                <a className="navbar-brand" href="#">
+                    <img src="/src/assets/icons/chart.png" width="30" height="30" class="brand" alt="chart"/>
+                    Rozliczenie 
+                </a>
+                <div className="nav">
+                    <a className="nav-link active">Transakcje</a>
+                    <a className="nav-link">Ustawienia</a>
+                    <a className="nav-link">Statystyki</a>
+                </div>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {App} from './components/App';
 import {render} from 'react-dom';
+import './main.css';
 
 render(<App/>, document.getElementById('app'));
